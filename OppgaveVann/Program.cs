8de8cc76c92bbcd2c1802eb6ffaafe3099ec1219
marti <marti@DESKTOP-K262RPL>
2023-04-03 14:00:40 +0200

    Add project files.

---
+++ OppgaveVann/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OppgaveVann
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
